f1f1e57a9aad9e3911e965425a3d31153adfd48e
Lignum <grgmckinlay@gmail.com>
2016-08-09 02:11:32 +0200

    Faster to_hex_string via lookup table

---
+++ include/nybbletohex.h
@@ -0,0 +1,3 @@
+#pragma once
+
+extern const char nybble_to_hex[16];
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,35 +1,34 @@
 #include "utils.h"
 #include "bytetohex.h"
+#include "nybbletohex.h"
 
 #include <openssl/sha.h>
 
 #include <cstdint>
 #include <cstring>
 
-#define TO_HEX(i) (char)((i) <= 9 ? '0' + (i) : 'a' - 10 + (i))
+inline char to_hex(int i) {
+	return nybble_to_hex[i];
+}
 
 void to_hex_string(uint64_t num, char str[17]) {
-	if (num <= 0xFFFFFFFFFFFFFFFF) {
-		str[0] = TO_HEX((num & 0xF000000000000000) >> 60);
-		str[1] = TO_HEX((num & 0x0F00000000000000) >> 56);
-		str[2] = TO_HEX((num & 0x00F0000000000000) >> 52);
-		str[3] = TO_HEX((num & 0x000F000000000000) >> 48);
-		str[4] = TO_HEX((num & 0x0000F00000000000) >> 44);
-		str[5] = TO_HEX((num & 0x00000F0000000000) >> 40);
-		str[6] = TO_HEX((num & 0x000000F000000000) >> 36);
-		str[7] = TO_HEX((num & 0x0000000F00000000) >> 32);
-		str[8] = TO_HEX((num & 0x00000000F0000000) >> 28);
-		str[9] = TO_HEX((num & 0x000000000F000000) >> 24);
-		str[10] = TO_HEX((num & 0x0000000000F00000) >> 20);
-		str[11] = TO_HEX((num & 0x00000000000F0000) >> 16);
-		str[12] = TO_HEX((num & 0x000000000000F000) >> 12);
-		str[13] = TO_HEX((num & 0x0000000000000F00) >> 8);
-		str[14] = TO_HEX((num & 0x00000000000000F0) >> 4);
-		str[15] = TO_HEX((num & 0x000000000000000F));
-		str[16] = '\0';
-	} else {
-		str[0] = '\0';
-	}
+	str[0] = nybble_to_hex[(num & 0xF000000000000000) >> 60];
+	str[1] = nybble_to_hex[(num & 0x0F00000000000000) >> 56];
+	str[2] = nybble_to_hex[(num & 0x00F0000000000000) >> 52];
+	str[3] = nybble_to_hex[(num & 0x000F000000000000) >> 48];
+	str[4] = nybble_to_hex[(num & 0x0000F00000000000) >> 44];
+	str[5] = nybble_to_hex[(num & 0x00000F0000000000) >> 40];
+	str[6] = nybble_to_hex[(num & 0x000000F000000000) >> 36];
+	str[7] = nybble_to_hex[(num & 0x0000000F00000000) >> 32];
+	str[8] = nybble_to_hex[(num & 0x00000000F0000000) >> 28];
+	str[9] = nybble_to_hex[(num & 0x000000000F000000) >> 24];
+	str[10] = nybble_to_hex[(num & 0x0000000000F00000) >> 20];
+	str[11] = nybble_to_hex[(num & 0x00000000000F0000) >> 16];
+	str[12] = nybble_to_hex[(num & 0x000000000000F000) >> 12];
+	str[13] = nybble_to_hex[(num & 0x0000000000000F00) >> 8];
+	str[14] = nybble_to_hex[(num & 0x00000000000000F0) >> 4];
+	str[15] = nybble_to_hex[(num & 0x000000000000000F)];
+	str[16] = '\0';
 }
 
 void sha256(const char *input, char *output) {
